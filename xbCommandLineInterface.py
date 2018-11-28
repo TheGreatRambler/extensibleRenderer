90fcc24fe9933a6fbb18939db4f1e1792ea4212e
TheGreatRambler <aehart684351@gmail.com>
2018-11-28 00:40:24 -0500

    Ascii art

--- xbCommandLineInterface.py
+++ xbCommandLineInterface.py
@@ -4,27 +4,38 @@ import xbRenderingProcess
 
 # TODO make this command line program loads better please!
 
+# the message to be displayed when seeing the version
+VERSION_MESSAGE = None
 VERSION = "0.0.1"
 
+def setVersionGraphic():
+	VERSION_GRAPHIC = None
+	with open("version_string.txt", "r", encoding="utf8") as versionString:
+		VERSION_GRAPHIC = versionString.read()
+	COPYRIGHT = "Copyright TheGreatRambler, MIT License, All rights reserved"
+	global VERSION_MESSAGE
+	VERSION_MESSAGE = VERSION_GRAPHIC + "\n" + VERSION + " " + COPYRIGHT
+setVersionGraphic()
+
 @click.group()
+@click.version_option(version=VERSION, message=VERSION_MESSAGE)
 def cli():
-    pass
+	pass
 
 @cli.command()
-@cli.option("-i", "--input-plugin", type=click.STRING, required=True)
-@cli.option("-o", "--output-plugin", type=click.STRING, required=True)
+@click.option("-i", "--input-plugin", type=click.STRING, required=True)
+@click.option("-o", "--output-plugin", type=click.STRING, required=True)
 def render(i, o):
 	pass
 
 @cli.command()
-@cli.option("-i", "--input-plugin", type=click.STRING, required=False)
-@cli.option("-o", "--output-plugin", type=click.STRING, required=False)
+@click.option("-i", "--input-plugin", type=click.STRING, required=False)
+@click.option("-o", "--output-plugin", type=click.STRING, required=False)
 def info(i, o):
 	# print info about plugins and exit
 	pass
 
 def setupClickFunctions():
-	click.version_option(VERSION)
 	cli()
 
 if __name__ == "__main__":
