e0d3228c37866f9427c107e6e6478bb2ef92f82c
TheGreatRambler <aehart684351@gmail.com>
2018-12-02 00:53:33 -0500

    Somewhat improved applications with guis in separate thread

--- plugins/Gp_renderWindow.py
+++ plugins/Gp_renderWindow.py
@@ -1,13 +1,13 @@
 import win32gui
 import win32ui
-import keyboard
 import ctypes
 import pywinauto
 from pywinauto.application import Application
+from pywinauto import Desktop
 from PIL import Image
 import subprocess
 import os
-import time
+import psutil
 # windows only solution
 # the way to get all applications (including jars and programs that start through the command line) is
 # by starting cmd.exe with this then manually putting in the command needed, listen for further windows,
@@ -19,17 +19,14 @@ class Main():
 	def __init__(self, command):
 		# we don't want cruft
 		pywinauto.actionlogger.disable()
-		# call command separately of pywinauto (redirection to DEVNULL prevents output)
-		self.DEVNULL = open(os.devnull, 'w')
-		self.process = subprocess.Popen(command, stdout=self.DEVNULL)
-		# try alt-enter
-		self.simulateAltEnter()
-		# open command window with existing command's PID
-		self.appInstance = Application(backend="win32").connect(process=self.process.pid)
+		# setup the application
+		# Output will say wether it failed or succeeded
+		applicationCreated = self.makeWorkingAppInstance(command)
 		# get the main/top window
-		self.mainWindow = self.appInstance.top_window()
+		# if application instance was not created, use the Desktop api
+		self.getMainWindow(command, applicationCreated)
 		# wait until the main window is active (maybe it should be enabled)
-		self.mainWindow.wait("enabled")
+		self.mainWindow.wait("visible")
 		# get screen size so window can be outside screen (TODO need to implement)
 		# just go for a massive value (last value is repaint ant 2 Nones are width and height)
 		self.appLocation = [3000, 3000]
@@ -67,15 +64,98 @@ class Main():
 		self.mfcDC.DeleteDC()
 		win32gui.ReleaseDC(self.hwnd, self.hwndDC)
 
+	def findProcessesByName(self, name):
+		ls = []
+		for p in psutil.process_iter():
+			# check if either the full path or just the filename has been created
+			if p.name() == name or p.name() == os.path.basename(name):
+				ls.append(p.pid)
+			'''
+			name_, exe, cmdline = "", "", []
+			try:
+				name_ = p.name()
+				cmdline = p.cmdline()
+				exe = p.exe()
+				print(name_, cmdline, exe)
+			except (psutil.AccessDenied, psutil.ZombieProcess):
+				pass
+			except psutil.NoSuchProcess:
+				continue
+			if name == name_ or cmdline[0] == name or os.path.basename(exe) == name:
+				ls.append(p.pid)
+			'''
+		return ls
+
+	def makeWorkingAppInstance(self, commandList):
+		# helper pipe to flush popen
+		self.DEVNULL = open(os.devnull, 'w')
+		# start the instance now
+		# sill be used many times throughout this function
+		self.appInstance = Application(backend="win32")
+		# first things first, lets try to create one from scratch
+		self.pid = subprocess.Popen(commandList, stdout=self.DEVNULL).pid
+		self.appInstance.connect(process=self.pid)
+		if len(self.appInstance.windows()) != 0:
+			# the app we created has some windows, we are done
+			self.usedExisting = True
+			# It worked
+			return True
+		else:
+			# We have to connect to an existing one
+			# kill the one we created, it is pointless
+			self.killPid(self.pid)
+			# find possible processes to use
+			possibleProcesses = self.findProcessesByName(commandList[0])
+			if len(possibleProcesses) == 0:
+				# goodness, the app is not running anywhere and it has no children
+				# this is a disaster
+				return False
+			else:
+				# check to see if it worked
+				foundWorkingOne = False
+				pidToUse = 0 # just a dummy number
+				for pid in possibleProcesses:
+					self.appInstance.connect(process=pid)
+					if len(self.appInstance.windows()) != 0:
+						# wow, this process has windows! Lets use it!
+						foundWorkingOne = True
+						pidToUse = pid
+						# we have what we need, lets get out of here
+						break
+				if foundWorkingOne:
+					# yay, register them. The application is already connected
+					self.pid = pidToUse
+					self.usedExisting = True
+					return True
+				else:
+					# I dont know what to do? There are places where this process is running
+					# But none of them have windows
+					return False
+
+	def getMainWindow(self, commandList, getAppWorked):
+		if getAppWorked is True:
+			self.mainWindow = self.appInstance.top_window()
+		else:
+			# start app seperately
+			self.usedExisting = False
+			self.pid = subprocess.Popen(commandList, stdout=self.DEVNULL).pid
+			# gets executable without extension
+			# most apps follow this pattern
+			self.mainWindow = Desktop(backend="win32").window(best_match=commandList[0].split(".")[0])
+
 	def kill(self):
 		self.breakdownScreenshotBitmap()
 		# kills given the pid
-		subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=self.process.pid), stdout=self.DEVNULL)
+		if not self.usedExisting:
+			# dont want to kill a process that was already running
+			self.killPid(self.pid)
+		else:
+			# maybe the old window should be restored?
+			pass
 
-	def simulateAltEnter(self):
-		keyboard.send("alt+enter")
+	def killPid(self, pid):
+		subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=pid), stdout=self.DEVNULL)
 		
-instance = Main(r"C:\Users\aehar\Desktop\Windows\GreekWarSaver.exe")
+instance = Main([r"C:\Windows\ImmersiveControlPanel\SystemSettings.exe"])
 instance.getScreenshot().save("thing1.png")
-instance.getScreenshot().save("thing2.png")
 instance.kill()
---
+++ xr.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import subprocess
+import ctypes
+
+# comes from https://stackoverflow.com/a/19719292/9329945
+
+def start(appType):
+	if appType == "gui":
+		pass
+	else:
+		# assume its command line
+		subprocess.call(["python", getCorrectLocation("xrCommandLineInterface.py")] + sys.argv[1:])
+
+def getCorrectLocation(file):
+	# concate this directory and the path to the file
+	return os.path.realpath(os.path.join(os.path.dirname(__file__), file))
+
+if __name__ == "__main__":
+	if len(sys.argv) == 1:
+		start("text")
+	else:
+		start(sys.argv[1])
+	sys.exit(0)
+
