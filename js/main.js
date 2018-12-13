11fb133b9240e961885d6b8dda6af88a5574fb39
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-12-13 10:59:21 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-var program = require("commander");
+const program = require("commander");
+const Worker = require("tiny-worker");
 
 versionString = "1.0.0";
 
 program
   .version(versionString)
+  .option("-r, --render", "Render frames (or a frame)")
+  .option("-I, --info", "Show info (overrides --render)")
+  .option("-i, --input-plugin", "Specify the rendering plugin name or absolute path")
   .parse(process.argv);
 
 // stuff here
